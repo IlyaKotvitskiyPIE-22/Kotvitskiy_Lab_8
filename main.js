c96177843d69c8bf9b3e3e893b0faeb3540580ef
IlyaKotvitskiyPIE-22 <152304737+IlyaKotvitskiyPIE-22@users.noreply.github.com>
2023-12-22 23:01:54 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,47 @@
-fetch('https://jsonplaceholder.typicode.com/posts')
-  .then((response) => response.json())
-  .then((data) => posts);
+document.addEventListener('DOMContentLoaded', function() {
+  const div_api_data = document.getElementById('api_data');
+
+  function objectToArray(dataObject) {
+    return Object.entries(dataObject);
+  }
+
+  function formatData(dataObject) {
+    let dataArray = objectToArray(dataObject);
+
+    for (let i = 0; i < dataArray.length; i++) {
+      let formatted = dataArray[i].join(': ');
+      dataArray[i] = formatted;
+    }
+
+    return dataArray;
+  }
+
+  function printData(dataObject) {
+    let formattedData = [];
+
+    for (let i = 0; i < dataObject.length; i++) {
+      formattedData.push(formatData(dataObject[i]));
+    }
+
+    for (let i = 0; i < formattedData.length; i++) {
+      let formattedString = formattedData[i].join(' | ');
+      let newDiv = document.createElement('div');
+      newDiv.textContent = formattedString;
+      div_api_data.appendChild(newDiv);
+
+      let lineBreak = document.createElement('br');
+      div_api_data.appendChild(lineBreak);
+    }
+  }
+
+  function getAllResource() {
+    const url = 'https://jsonplaceholder.typicode.com/posts';
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        printData(data);
+      })
+  }
+
+  getAllResource(); // Вызов функции при загрузке страницы
+});
